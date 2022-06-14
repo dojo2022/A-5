ddaac442307ffdeb7831921489703d8d2ddc6a0c
dojo2022-kageyama <103239405+dojo2022-kageyama@users.noreply.github.com>
2022-06-14 10:14:59 +0900

    create_tableを更新

--- doc/create_table.sql
+++ doc/create_table.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(15) NOT NULL,
+    user_name VARCHAR(15) NOT NULL UNIQUE,
     user_password VARCHAR(32) NOT NULL,
     priority_calendar INT DEFAULT 0 NOT NULL,
     is_delete BOOLEAN DEFAULT FALSE NOT NULL
