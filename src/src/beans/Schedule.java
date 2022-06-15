c6cd8583450d119a199bca483de20799381e974d
dojo2022-kageyama <103239405+dojo2022-kageyama@users.noreply.github.com>
2022-06-15 13:45:00 +0900

    beansの改訂版

--- src/src/beans/Schedule.java
+++ src/src/beans/Schedule.java
@@ -1,5 +1,72 @@
 package beans;
 
+import java.sql.Date; //質問
+import java.sql.Time;
+
 public class Schedule {
+
+
+
+	public Schedule() {
+		super();
+	}
+	private int schedule_id;
+	private String schedule;
+	private String schedule_type;
+	private Date date;
+	private Time time;
+	private String memo;
+	private int calendar_id;
+	private Date last_date;
+	public int getSchedule_id() {
+		return schedule_id;
+	}
+	public void setSchedule_id(int schedule_id) {
+		this.schedule_id = schedule_id;
+	}
+	public String getSchedule() {
+		return schedule;
+	}
+	public void setSchedule(String schedule) {
+		this.schedule = schedule;
+	}
+	public String getSchedule_type() {
+		return schedule_type;
+	}
+	public void setSchedule_type(String schedule_type) {
+		this.schedule_type = schedule_type;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public Time getTime() {
+		return time;
+	}
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public int getCalendar_id() {
+		return calendar_id;
+	}
+	public void setCalendar_id(int calendar_id) {
+		this.calendar_id = calendar_id;
+	}
+	public Date getLast_date() {
+		return last_date;
+	}
+	public void setLast_date(Date last_date) {
+		this.last_date = last_date;
+	}
+
+
 
 }
