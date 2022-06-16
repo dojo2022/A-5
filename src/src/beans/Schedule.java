cb1bd86ac7bf38f7475e584cc6896d2649ff53e1
dojo2022-kageyama <103239405+dojo2022-kageyama@users.noreply.github.com>
2022-06-16 15:16:40 +0900

    スケジュールビーンズ更新

--- src/src/beans/Schedule.java
+++ src/src/beans/Schedule.java
@@ -1,72 +1,86 @@
 package beans;
 
-
 import java.util.Date;
 
 public class Schedule {
-
-
+	private int scheduleId;
+	private String schedule;
+	private String scheduleType;
+	private Date date;
+	private Date time;
+	private String memo;
+	private int calendarId;
+	private Date lastDate;
 
 	public Schedule() {
 		super();
 	}
-	private int schedule_id;
-	private String schedule;
-	private String schedule_type;
-	private Date date;
-	private Date time;
-	private String memo;
-	private int calendar_id;
-	private Date last_date;
-	public int getSchedule_id() {
-		return schedule_id;
+
+	public int getScheduleId() {
+		return scheduleId;
 	}
-	public void setSchedule_id(int schedule_id) {
-		this.schedule_id = schedule_id;
+
+	public void setScheduleId(int scheduleId) {
+		this.scheduleId = scheduleId;
 	}
+
 	public String getSchedule() {
 		return schedule;
 	}
+
 	public void setSchedule(String schedule) {
 		this.schedule = schedule;
 	}
-	public String getSchedule_type() {
-		return schedule_type;
+
+	public String getScheduleType() {
+		return scheduleType;
 	}
-	public void setSchedule_type(String schedule_type) {
-		this.schedule_type = schedule_type;
+
+	public void setScheduleType(String scheduleType) {
+		this.scheduleType = scheduleType;
 	}
+
 	public Date getDate() {
 		return date;
 	}
+
 	public void setDate(Date date) {
+
 		this.date = date;
 	}
+
 	public Date getTime() {
 		return time;
 	}
+
 	public void setTime(Date time) {
+
 		this.time = time;
 	}
+
 	public String getMemo() {
 		return memo;
 	}
+
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
-	public int getCalendar_id() {
-		return calendar_id;
-	}
-	public void setCalendar_id(int calendar_id) {
-		this.calendar_id = calendar_id;
-	}
-	public Date getLast_date() {
-		return last_date;
-	}
-	public void setLast_date(Date last_date) {
-		this.last_date = last_date;
+
+	public int getCalendarId() {
+		return calendarId;
 	}
 
+	public void setCalendarId(int calendarId) {
+		this.calendarId = calendarId;
+	}
 
+	public Date getLastDate() {
+		return lastDate;
+	}
+
+	public void setLastDate(Date lastDate) {
+
+		this.lastDate = lastDate;
+	}
 
 }
