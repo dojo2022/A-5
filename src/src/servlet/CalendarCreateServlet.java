c2c3b7c19ee2c7f81e1a403018a66a31feea9555
dojo2022-ishigai <103239411+dojo2022-ishigai@users.noreply.github.com>
2022-06-16 16:19:19 +0900

    CalendarCreateServletの作成

--- src/src/servlet/CalendarCreateServlet.java
+++ src/src/servlet/CalendarCreateServlet.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import beans.CalendarBeans;
+import beans.User;
+import dao.CalendarsDAO;
+import logic.ValidationLogic;
+
 /**
  * Servlet implementation class CalendarCreateServlet
  */
@@ -23,13 +28,13 @@ public class CalendarCreateServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//ログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
-		if (session.getAttribute("user") == null) {
+		if (session.getAttribute("loginUser") == null) {
 			response.sendRedirect("/machico/LoginServlet");
 			return;
 		}
 
 		//フォワード先のjsp書き換え
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accountCreate.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/calendarCreate.jsp");
 		dispatcher.forward(request, response);
 	}
 
@@ -37,8 +42,50 @@ public class CalendarCreateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//ログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		if (session.getAttribute("loginUser") == null) {
+			response.sendRedirect("/machico/LoginServlet");
+			return;
+		}
+
+		//calendarCreate.jspから入力されたカレンダータイトルを取得
+		request.setCharacterEncoding("UTF-8");
+		String title = request.getParameter("title-textbox");
+
+		//バリデーションをインポート(要確認！！)
+		//Boolean vl = ValidationLogic.checkCalendarName(title);
+
+		if(ValidationLogic.checkCalendarName(title) ==false) {
+			request.setAttribute("errMessage", "カレンダー追加できませんでした");
+			//カレンダー追加画面に戻る処理かく？
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/calendarCreate.jsp");
+			dispatcher.forward(request, response);
+		}
+
+		//カレンダー登録処理↓↓
+
+		//DAO呼ぶ
+		CalendarsDAO cDao = new CalendarsDAO();
+
+		//ログインしているユーザーを呼ぶ
+		User user =(User) session.getAttribute("loginUser");
+
+		//Beansにタイトル入れる
+		CalendarBeans cb = new CalendarBeans();
+		cb.setCalendarName(title);
+
+		if(cDao.insertCalendar(cb, user) == true) {
+			//カレンダーにリダイレクトする
+			response.sendRedirect("/machico/CalendarServlet");
+		}else {
+			//DAOではじかれたときのメッセージ表示
+			request.setAttribute("errMessage", "カレンダー追加できませんでした");
+			//カレンダー追加画面に戻る処理
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/calendarCreate.jsp");
+			dispatcher.forward(request, response);
+		}
+
 	}
 
 }
