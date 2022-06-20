c36298a4b4182fcce2c4e8aa7b0c39c368417d3b
dojo2022-oki <103237572+dojo2022-oki@users.noreply.github.com>
2022-06-20 09:46:44 +0900

    firstClendarCreateのファイル作成

--- src/src/servlet/FirstCalendarCreateServlet.java
+++ src/src/servlet/FirstCalendarCreateServlet.java
@@ -8,6 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import beans.CalendarBeans;
+import beans.User;
+import dao.UserDAO;
+import logic.ValidationLogic;
 
 /**
  * Servlet implementation class FirstCalendarCreateServlet
@@ -19,17 +25,65 @@ public class FirstCalendarCreateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accountCreate.jsp");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/firstCalendarCreate.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String new_name = request.getParameter("new_user_name_textbox");
+		String new_pw = request.getParameter("new_password_textbox");
+		String new_title = request.getParameter("new_title_textbox");
+
+		UserDAO uDao = new UserDAO();
+
+		if (!ValidationLogic.checkCalendarName(new_title)) {
+			request.setAttribute("errMessage", "入力内容が間違っています");
+			//最初のカレンダー画面にもどるよ
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/firstCalendarCreate.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			//Beansにタイトル入れる
+			CalendarBeans cb = new CalendarBeans();
+			cb.setCalendarName(new_title);
+
+			if (uDao.createAccount(new_name, new_pw, cb)) {
+				//Beansからとってきたidとnameを u に入れて持ってくる
+				User u = uDao.isLoginOK(new_name, new_pw);
+				// ログイン成功
+				if (u != null) {
+					// セッションスコープにIDを格納する
+					HttpSession session = request.getSession();
+					session.setAttribute("loginUser", u);
+
+					// マス目カレンダーにフォワードする
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/gridCalendar.jsp");
+					dispatcher.forward(request, response);
+				} else {
+					// リクエストスコープにエラーメッセージを格納する
+					request.setAttribute("errMessage", "エラーが発生しました。アカウントは作成済です。");
+
+					// ログイン画面にフォワードする
+					RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+					dispatcher.forward(request, response);
+				}
+
+			} else {
+				//アカウント登録失敗
+				request.setAttribute("errMessage", "アカウント作成に失敗しました。");
+				//カレンダー追加画面に戻る処理
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/firstCalendarCreate.jsp");
+				dispatcher.forward(request, response);
+			}
+		}
 	}
 
 }
