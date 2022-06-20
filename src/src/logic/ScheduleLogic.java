0362a2ad7f14c46569802c4ab38f49b338f8baad
dojo2022-kageyama <103239405+dojo2022-kageyama@users.noreply.github.com>
2022-06-20 13:50:18 +0900

    スケジュールロジック作成

--- src/src/logic/ScheduleLogic.java
+++ src/src/logic/ScheduleLogic.java
@@ -1,5 +1,47 @@
 package logic;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import beans.CalendarBeans;
+import beans.Schedule;
+import dao.SchedulesDAO;
+
 public class ScheduleLogic {
+
+	public static void main(String[] args) {
+		SchedulesDAO sdao = new SchedulesDAO();
+		Schedule s = new Schedule();
+		CalendarBeans cb = new CalendarBeans();
+		ScheduleCompile(sdao.select(s, cb));
+
+	}
+
+	//メインをただのメソッドにしたとき引数でデータをもらう
+	public static List<ArrayList<Schedule>> ScheduleCompile(List<Schedule> ScheduleList) {
+
+		List<ArrayList<Schedule>> scheduleData = new ArrayList<ArrayList<Schedule>>();
+
+		//最初に件数が0件のArrayList配列をつくるを30件作ってそこに入れる
+		for (int i = 0; i < 30; i++) {
+
+			scheduleData.add(i, new ArrayList<Schedule>());
+
+		}
+		//スケジュールリストから日付取得、ArrayListの番号に対応させていれる
+		for (int i = 0; i < ScheduleList.size(); i++) {
+
+			Date day = ScheduleList.get(i).getDate();
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(day);
+			int schDay = cal.get(Calendar.DAY_OF_MONTH) - 1;
+			scheduleData.get(schDay).add(ScheduleList.get(i));
+
+		}
+		return scheduleData;
+
+	}
 
 }
