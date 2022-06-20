82bb6d3078c619d98c534f20529b3615f3146e38
dojo2022-oki <103237572+dojo2022-oki@users.noreply.github.com>
2022-06-20 09:46:21 +0900

    accountCreateのファイル作成

--- src/src/servlet/AccountCreateServlet.java
+++ src/src/servlet/AccountCreateServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.UserDAO;
+import logic.ValidationLogic;
+
 /**
  * Servlet implementation class AccountCreateServlet
  */
@@ -19,7 +22,8 @@ public class AccountCreateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accountCreate.jsp");
 		dispatcher.forward(request, response);
 	}
@@ -27,9 +31,36 @@ public class AccountCreateServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+			// リクエストパラメータを取得する
+			request.setCharacterEncoding("UTF-8");
+			String new_name = request.getParameter("new_user_name_textbox");
+			String new_pw = request.getParameter("new_password_textbox");
+
+			UserDAO uDao = new UserDAO();
+
+			if (!ValidationLogic.checkUserName(new_name)) {
+				request.setAttribute("errMessage", "半角英数字と_のみで入力してください");
+				//アカウント作成画面にもどるよ
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accountCreate.jsp");
+				dispatcher.forward(request, response);
+			} else if (!uDao.isExistUser(new_name)) {
+				request.setAttribute("errMessage", "このアカウントは使用されています");
+				//アカウント作成画面にもどるよ
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accountCreate.jsp");
+				dispatcher.forward(request, response);
+			} else {
+				// リクエスト領域（Attribute）に保存する
+				request.setAttribute("new_name", new_name);
+				request.setAttribute("new_pw", new_pw);
+
+				// 最初のカレンダー作成画面にフォワードする
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/firstCalendarCreate.jsp");
+				dispatcher.forward(request, response);
+			}
+
+
 	}
 
 }
