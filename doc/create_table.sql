34e73d4077d00996b3d48dbbaad503d034dfe7ce
dojo2022-kageyama <103239405+dojo2022-kageyama@users.noreply.github.com>
2022-06-23 16:34:08 +0900

    テーブルメモ更新

--- doc/create_table.sql
+++ doc/create_table.sql
@@ -51,4 +51,4 @@ INSERT INTO calendars ( calendar_name, user_id) VALUES ('テストカレンダ�
 INSERT INTO schedules ( schedule, schedule_type, date, time, last_time, memo , calendar_id,last_date,auto_last_date) VALUES ('てすと', 'F',
 TO_DATE('22-02-11', 'YY-MM-DD') ,null , null,'aaaa', 1, TO_DATE('22-02-12', 'YY-MM-DD') , null);
 INSERT INTO regular_schedules (schedule , first_date , last_date , memo , calendar_id , regular_schedule_type ,regular_schedule_value)
-VALUES('定期てすと' , TO_DATE('22-02-11', 'YY-MM-DD') , TO_DATE('22-02-12', 'YY-MM-DD') ,'めもてすと',1,'W',' 1 , 6')
+VALUES('定期てすと' , TO_DATE('22-02-11', 'YY-MM-DD') , TO_DATE('22-02-12', 'YY-MM-DD') ,'めもてすと',1,'W','1,6')
