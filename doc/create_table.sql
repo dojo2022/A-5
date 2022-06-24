409922f4c3a2c9d5a689624de88d8ca39d1fbd0f
dojo2022-kageyama <103239405+dojo2022-kageyama@users.noreply.github.com>
2022-06-24 09:32:48 +0900

    DBテーブル作成予定数追加更新

--- doc/create_table.sql
+++ doc/create_table.sql
@@ -49,6 +49,10 @@ CREATE TABLE regular_schedules (
 INSERT INTO users (user_name,user_password) VALUES ('test' ,'test0000');
 INSERT INTO calendars ( calendar_name, user_id) VALUES ('テストカレンダー', 1);
 INSERT INTO schedules ( schedule, schedule_type, date, time, last_time, memo , calendar_id,last_date,auto_last_date) VALUES ('てすと', 'F',
-TO_DATE('22-02-11', 'YY-MM-DD') ,null , null,'aaaa', 1, TO_DATE('22-02-12', 'YY-MM-DD') , null);
+TO_DATE('22-06-16', 'YY-MM-DD') ,null , null,'aaaa', 1, TO_DATE('22-06-18', 'YY-MM-DD') , null);
+INSERT INTO schedules ( schedule, schedule_type, date, time, last_time, memo , calendar_id,last_date,auto_last_date) VALUES ('てすと', 'A',
+TO_DATE('22-06-26', 'YY-MM-DD') ,null , null,'aaaa', 1, TO_DATE('22-06-26', 'YY-MM-DD') , null);
+INSERT INTO schedules ( schedule, schedule_type, date, time, last_time, memo , calendar_id,last_date,auto_last_date) VALUES ('てすと', 'A',
+TO_DATE('22-06-29', 'YY-MM-DD') ,null , null,'aaaa', 1, TO_DATE('22-06-29', 'YY-MM-DD') , null);
 INSERT INTO regular_schedules (schedule , first_date , last_date , memo , calendar_id , regular_schedule_type ,regular_schedule_value)
 VALUES('定期てすと' , TO_DATE('22-02-11', 'YY-MM-DD') , TO_DATE('22-02-12', 'YY-MM-DD') ,'めもてすと',1,'W','1,6')
